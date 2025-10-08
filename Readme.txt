a047c726d31bbfa44cc0feb71b3ca4344278ed31
DeepakAIMLQ <74923940+DeepakAIMLQ@users.noreply.github.com>
2025-10-08 22:11:11 +0530

    add readme.txt file

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+-- Run Dependencies
+>> pip install -r requirements.txt
+
+-- Run FrontEnd 
+>> streamlit run test_client.py
+
+-- Run Backend
+
+>> uvicorn app.main:app --reload
--- test_client.py
+++ test_client.py
@@ -5,7 +5,7 @@ import io
 import os
 
 #Update this with your backend public URL after deployment
-FASTAPI_URL = "https://pii-backend-i8bg.onrender.com/extract"
+FASTAPI_URL = "http://127.0.0.1:8000/extract"
 
 st.set_page_config(page_title="📤 PII Extractor POC", layout="centered")
 st.title("🔎 PII/PHI Extraction – FastAPI POC")
